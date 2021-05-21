9f06f7d7075bc395c230153515efa9812df3985e
choimory <choimory@naver.com>
2021-05-21 18:39:19 +0900

    Initial commit

---
+++ src/test/java/com/practice/hellomapstruct/HellomapstructApplicationTests.java
@@ -0,0 +1,13 @@
+package com.practice.hellomapstruct;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HellomapstructApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
